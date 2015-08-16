7ebcf85779a3888f88be1b7db4ea8a42ec0492cc
Sakshi <khanna.sakshi09@gmail.com>
2015-08-17 00:18:19 +0530

    added watch,sass plugin iin gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,41 @@
 module.exports = function(grunt) {
+	require('load-grunt-tasks')(grunt);
 	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 	  connect: {
 	    server: {
 	      options: {
 	        port: 9001,
 	        base: 'src',
-	        keepalive: true
+	        // keepalive: true
 	      }
 	    }
-	  }
+	  },
+	  sass: {
+	    dist: {
+	      options: {
+	        style: 'expanded',
+	        trace: true
+	      },
+	      files: {
+	        'src/css/compiled.css': 'src/scss/main.scss'
+	      }
+	    }
+	  },
+	  watch: {
+	   css: {
+	       options: {
+	         livereload: true,
+
+	       },
+	       files: 'src/scss/**/*.scss',
+	       tasks: ['sass'],
+	     }
+	  }	  
 	});
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.registerTask('start-dev', ['connect']);
-}
+	// grunt.loadNpmTasks('grunt-contrib-watch');
+	// grunt.loadNpmTasks('grunt-contrib-connect');
+	// grunt.loadNpmTasks('grunt-contrib-sass');
+	// grunt.registerTask('convert', ['sass','connect','watch']);
+	grunt.registerTask('start-dev', ['sass','connect','watch']);
+};
