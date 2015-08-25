1c63998e6b2072962b62cdc211011e13ca4d58b0
Sakshi <khanna.sakshi09@gmail.com>
2015-08-25 23:34:05 +0530

    added focus event on btn

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,18 @@
-// var domains = ['hotmail.com', 'gmail.com', 'aol.com'];
-//     var topLevelDomains = ["com", "net", "org"];
-
-//     $('#email').on('blur', function(event) {
-//       console.log("event ", event);
-//       console.log("this ", $(this));
-//       $(this).mailcheck({
-//         domains: domains,                       // optional
-//         topLevelDomains: topLevelDomains,       // optional
-//         suggested: function(element, suggestion) {
-//           // callback code
-//           console.log("suggestion ", suggestion.full);
-//           $('#suggestion').html("Did you mean <b><i>" + suggestion.full + "</b></i>?");
-//         },
-//         empty: function(element) {
-//           // callback code
-//           $('#suggestion').html('No Suggestions :(');
-//         }
-//       });
-//     });
-
-
-
-    $("#email").validEmail({on:"blur", success:function(){
+  $("#email").validEmail({on:"blur", success:function(){
       $(this).removeClass("error");
       $('.email-validation p').empty();
     }, failure:function(){
       // console.log('2');
       $(this).addClass("error");
       $('.email-validation p').text('Invalid Email Id');
+      
     }});
+
+$( ".btn-container a" ).focus(function(){
+    console.log('focus');
+    $( ".btn-container .subscribe-btn" ).css( "background-color", "#ffc52f" );
+  });
+$( ".btn-container a" ).blur(function(){
+    console.log('blur');
+    $( ".btn-container .subscribe-btn" ).css( "background-color", "#ffd15d" );
+  });
