23935efefdb076a5015ed5570fb5608734de2c0c
Sakshi <khanna.sakshi09@gmail.com>
2015-08-25 21:57:50 +0530

    added library for email validation

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+// var domains = ['hotmail.com', 'gmail.com', 'aol.com'];
+//     var topLevelDomains = ["com", "net", "org"];
+
+//     $('#email').on('blur', function(event) {
+//       console.log("event ", event);
+//       console.log("this ", $(this));
+//       $(this).mailcheck({
+//         domains: domains,                       // optional
+//         topLevelDomains: topLevelDomains,       // optional
+//         suggested: function(element, suggestion) {
+//           // callback code
+//           console.log("suggestion ", suggestion.full);
+//           $('#suggestion').html("Did you mean <b><i>" + suggestion.full + "</b></i>?");
+//         },
+//         empty: function(element) {
+//           // callback code
+//           $('#suggestion').html('No Suggestions :(');
+//         }
+//       });
+//     });
+
+
+
+    $("#email").validEmail({on:"blur", success:function(){
+      $(this).removeClass("error");
+    }, failure:function(){
+      // console.log('2');
+      $(this).addClass("error");
+    }});
