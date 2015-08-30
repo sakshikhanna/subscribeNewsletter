f9b25e0ecdb7c49d74b0da8db637c68951e247c2
Sakshi <khanna.sakshi09@gmail.com>
2015-08-30 18:21:20 +0530

    validate email on click of a btn

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,11 @@
-  $("#email").validEmail({on:"blur", success:function(){
+  $("#email").validEmail({on:"blur", success:function(event){
       $(this).removeClass("error");
       $('.email-validation p').empty();
     }, failure:function(){
       // console.log('2');
       $(this).addClass("error");
       $('.email-validation p').text('Invalid Email Id');
+      // event.preventDefault();
       
     }});
 
@@ -14,5 +15,18 @@ $( ".btn-container a" ).focus(function(){
   });
 $( ".btn-container a" ).blur(function(){
     console.log('blur');
-    $( ".btn-container .subscribe-btn" ).css( "background-color", "#ffd15d" );
+    $( ".btn-container .subscribe-btn" ).css( "background-color", "#FFDE8D" );
   });
+$(".subscribe-btn").click(function(event){
+  var emailValue = $(".email-text");
+  if(emailValue.val().length === 0){
+     $("#email").addClass("error");
+     $('.email-validation p').text('Enter Valid Email Address');
+     event.preventDefault();
+  }else{
+    if($("#email").hasClass("error")){
+      event.preventDefault();
+    }      
+  }
+})
+
